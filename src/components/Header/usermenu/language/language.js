29e502fda81904d8cc24d488ac7cc4673a0389d5
Dzianis <dzianis888mv@gmail.com>
2022-11-20 17:31:36 +0300

    Добавіл обработчік сабытій

--- src/components/Header/usermenu/language/language.js
+++ src/components/Header/usermenu/language/language.js
@@ -1,14 +1,25 @@
 import React  from 'react';
+import * as ReactDOM from 'react-dom';
 import "./language.css";
 import image from "../../../../assets/svg/Flag_UK.svg";
 
+
 //let image = require('../../assets/img/bel.jpg');
+let test = "Тест";
+
+
+function activateLasers(){
+  console.log("Здесь");
+  alert(test);
+};
+
+
 
 export const Language = () => {
     return (
       <>
-       <div className='belPosition'>
-          <img class="bel" src={image} alt="some example image"></img>
+       <div onClick={activateLasers} className='belPosition'>
+          <img   class="bel" src={image} alt="some example image"></img>
         </div>
       </>
     );
