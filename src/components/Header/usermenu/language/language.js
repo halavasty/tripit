4bea5bf30ee741244add3f99ae47e8ff608f384d
Dzianis <dzianis888mv@gmail.com>
2022-11-20 17:55:20 +0300

    Добавил обработчик событий

--- src/components/Header/usermenu/language/language.js
+++ src/components/Header/usermenu/language/language.js
@@ -6,11 +6,23 @@ import image from "../../../../assets/svg/Flag_UK.svg";
 
 //let image = require('../../assets/img/bel.jpg');
 let test = "Тест";
+let lisLungudg = "lisLungudg";
 
+
+const mainHTML = `
+  <ul  class="${lisLungudg}">
+    <li>Мова</li>
+    <li>Мова</li>
+    <li>Мова</li>
+  </ul>
+`;
 
 function activateLasers(){
   console.log("Здесь");
-  alert(test);
+  //alert(test);
+
+  const textMainGo = document.querySelector("#textMainGo");
+  textMainGo.insertAdjacentHTML("afterend", mainHTML);
 };
 
 
@@ -21,6 +33,10 @@ export const Language = () => {
        <div onClick={activateLasers} className='belPosition'>
           <img   class="bel" src={image} alt="some example image"></img>
         </div>
+        
+        <div id='textMainGo'>
+        </div>
+
       </>
     );
   };
