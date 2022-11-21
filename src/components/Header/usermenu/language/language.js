8d58f92dd6f00380c3342e94aa75e01254b2421a
Dzianis <dzianis888mv@gmail.com>
2022-11-22 02:25:10 +0300

    Доработка кода

--- src/components/Header/usermenu/language/language.js
+++ src/components/Header/usermenu/language/language.js
@@ -1,177 +1,115 @@
-import React from 'react';
+import React from "react";
 import "./language.css";
 import image from "../../../../assets/svg/Flag_UK.svg";
 import imageBel from "../../../../assets/img/bel.jpg";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-let lisLungudg = "lisLungudg";
-let dellClass = "dellClass";
-
- const mainHTML = `
- 
-              `;
-
-let dev1 ;
-// 
-let dev = image;
- let devTwo = imageBel;
- 
-let a = 0;
-function activateLasers(){
-  console.log("fdsfsd");
-  // console.log(activateLasers);
-  console.log("dsfsdfsadfdsfsadfsd");
-let a = "onClick={activateLasersBel}";
+function activateLasers() {
 
   function createMarkup() {
-    return {__html: `  <div id='textMainGo' onClick={activateLasers}  class="selecting-lang"> <img  class="lang-flag" src=${image}/></div>`};
+    return {
+      __html: `  <div id='textMainGo' onClick={activateLasers}  class="selecting-lang"> <img  class="lang-flag" src=${image}/></div>`,
+    };
   }
 
-  function createMarkup5() {
-    return {__html: `  <div id='textMainGoTwo'  onClick={activateLasersBel} class="selecting-lang"> <img  class="lang-flag" src=${imageBel}/></div>`};
+  function createFoor() {
+    return {
+      __html: `  <div id='textMainGoTwo'  onClick={activateLasersBel} class="selecting-lang"> <img  class="lang-flag" src=${imageBel}/></div>`,
+    };
   }
 
-  ++a;
-  console.log(a);
-  const root = ReactDOM.createRoot(document.getElementById('textMainGo'));
-  const rootTwo = ReactDOM.createRoot(document.getElementById('textMainGoTwo'));
- 
+  const root = ReactDOM.createRoot(document.getElementById("textMainGo"));
+  const rootTwo = ReactDOM.createRoot(document.getElementById("textMainGoTwo"));
+
   let dev = <div dangerouslySetInnerHTML={createMarkup()} />;
-  let devTwo = <div dangerouslySetInnerHTML={createMarkup5()} />;
+  let devTwo = <div dangerouslySetInnerHTML={createFoor()} />;
   root.render(dev);
   rootTwo.render(devTwo);
-
-
-
 }
-let ab = 1;
 
-function activateLasersBel(){
- // alert("флаг 2");
+let count = 1;
 
+function activateLasersBel() {
+  // alert("флаг 2");
 
-if(ab === 1){
- let a = "onClick={activateLasersBel}";
- let b = "onClick={activateLasers}"
+  if (count === 1) {
+    let a = "onClick={activateLasersBel}";
+    let b = "onClick={activateLasers}";
 
- function createMarkup3() {
-  return {__html: `
+    function createThree() {
+      return {
+        __html: `
        
-  <div id='textMainGo' ${a} class="selecting-lang"><img  class="lang-flag" src=${image}/>
-      
-  `};
-}
+                <div id='textMainGo' ${a} class="selecting-lang"><img  class="lang-flag" src=${image}/>
+                    
+                `,
+      };
+    }
 
-  function createMarkup2() {
-    return {__html: `
-         
+    function createTwo() {
+      return {
+        __html: `
               <div id='textMainGo' ${b} class="selecting-lang"><img  class="lang-flag" src=${imageBel}/></div>
-        
-    `};
+               `,
+      };
+    }
+
+    const root = ReactDOM.createRoot(document.getElementById("textMainGo"));
+    const rootTwo = ReactDOM.createRoot(
+      document.getElementById("textMainGoTwo")
+    );
+    let devTwo = <div dangerouslySetInnerHTML={createThree()} />;
+    let dev = <div dangerouslySetInnerHTML={createTwo()} />;
+
+    root.render(dev);
+    rootTwo.render(devTwo);
+    ++count;
+  } else {
+    function createMarkup() {
+      return {
+        __html: `  <div id='textMainGo' onClick={activateLasers}  class="selecting-lang"> <img  class="lang-flag" src=${image}/></div>`,
+      };
+    }
+
+    function createMarkup5() {
+      return {
+        __html: `  <div id='textMainGoTwo'  onClick={activateLasersBel} class="selecting-lang"> <img  class="lang-flag" src=${imageBel}/></div>`,
+      };
+    }
+
+    const root = ReactDOM.createRoot(document.getElementById("textMainGo"));
+    const rootTwo = ReactDOM.createRoot(
+      document.getElementById("textMainGoTwo")
+    );
+
+    let dev = <div dangerouslySetInnerHTML={createMarkup()} />;
+    let devTwo = <div dangerouslySetInnerHTML={createMarkup5()} />;
+    root.render(dev);
+    rootTwo.render(devTwo);
+    return (count = 1);
   }
-  
-
-  console.log(a);
-  const root = ReactDOM.createRoot(document.getElementById('textMainGo'));
-  const rootTwo = ReactDOM.createRoot(document.getElementById('textMainGoTwo'));
-  let devTwo = <div dangerouslySetInnerHTML={createMarkup3()} />;
-  let dev = <div dangerouslySetInnerHTML={createMarkup2()} />;
-
-  root.render(dev);
-  rootTwo.render(devTwo);
- ++ab;
-}else{
-  function createMarkup() {
-    return {__html: `  <div id='textMainGo' onClick={activateLasers}  class="selecting-lang"> <img  class="lang-flag" src=${image}/></div>`};
-  }
-
-  function createMarkup5() {
-    return {__html: `  <div id='textMainGoTwo'  onClick={activateLasersBel} class="selecting-lang"> <img  class="lang-flag" src=${imageBel}/></div>`};
-  }
-
-
-  console.log(a);
-  const root = ReactDOM.createRoot(document.getElementById('textMainGo'));
-  const rootTwo = ReactDOM.createRoot(document.getElementById('textMainGoTwo'));
- 
-  let dev = <div dangerouslySetInnerHTML={createMarkup()} />;
-  let devTwo = <div dangerouslySetInnerHTML={createMarkup5()} />;
-  root.render(dev);
-  rootTwo.render(devTwo);
-  return ab = 1;
 }
-
-}
-
-
-// function Mycom(){
-
-//   const root = ReactDOM.createRoot(
-//     document.getElementById('root')
-//   );
-//   const element = <h1>Hello, world</h1>;
-//   root.render(element);
-// }
-
-// function MyComponent() {
-
-//   return <div dangerouslySetInnerHTML={activateLasers()} />;
-                   
-// }
-
-
-
-// 
-// function MyComponent() {
-//   return <div dangerouslySetInnerHTML={createMarkup()} />;
-// }
-
-// let count = 0;
-
-// function activateLasers(){
-
-
-//   if(count === 0){
-//   const textMainGo = document.querySelector("#textMainGo");
-//   textMainGo.insertAdjacentHTML("afterend", mainHTML);
-//   count = 1;
-//   }else{
-//   let elem = document.querySelector("#dellClass");
-//   if(elem != null){
-//   elem.remove();
-//   }
-//   count = 0;
-//   }
-// };
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<Language />, rootElement);
-
-
 
 export const Language = () => {
-    return (
-      <>
-       <div  className='belPosition'>
-       <div class="switch-lang"> 
-         
-          <div id='textMainGo' onClick={activateLasers} class="selecting-lang"><img  class="lang-flag" src={image}/>
-        </div>
-        <div  class="lang-dropdown">
-          <div id='textMainGoTwo' onClick={activateLasersBel} class="selecting-lang"><img  class="lang-flag" src={imageBel}/>
-       
+  return (
+    <>
+      <div className="belPosition">
+        <div class="switch-lang">
+          <div id="textMainGo" onClick={activateLasers} class="selecting-lang">
+            <img class="lang-flag" src={image} />
+          </div>
+          <div class="lang-dropdown">
+            <div
+              id="textMainGoTwo"
+              onClick={activateLasersBel}
+              class="selecting-lang"
+            >
+              <img class="lang-flag" src={imageBel} />
+            </div>
           </div>
         </div>
-        </div>
-         
-        </div>
-        <p  >{dev1}</p>
-        {/* <MyComponent/> */}
-
-
-
-      </>
-
-      
-    
-    );
-  };
+      </div>
+  
+    </>
+  );
+};
